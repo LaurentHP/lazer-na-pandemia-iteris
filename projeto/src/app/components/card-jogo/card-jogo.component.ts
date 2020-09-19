ae512cf0d60661cf230394ce2e578ab28437511d
unknown <laurent_paterlini98@hotmail.com>
2020-09-19 14:50:24 -0300

    Atualização

---
+++ projeto/src/app/components/card-jogo/card-jogo.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { JogosModel } from 'src/app/services/jogosApi/jogos-model/jogos-model';
+
+@Component({
+  selector: 'app-card-jogo',
+  templateUrl: './card-jogo.component.html',
+  styleUrls: ['./card-jogo.component.css']
+})
+export class CardJogoComponent implements OnChanges {
+
+  @Input() jogo: JogosModel;
+
+  imgUrl: string;
+  genre:string;
+  name:string;
+  plataform:string;
+  year:number;
+
+
+
+  constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.jogo!= null) {
+
+      this.imgUrl = this.jogo.photo;
+      this.genre = this.jogo.genre;
+      this.name = this.jogo.name;
+      this.plataform = this.plataform;
+      this.year = this.jogo.year;
+
+  }
+  }
+
+
+
+}
